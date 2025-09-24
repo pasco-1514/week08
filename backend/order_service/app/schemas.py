ea92da5bfdaa607b8a305c86d8fc99befbee64ff
pasco-1514 <pascobett1514@gmail.com>
2025-09-24 18:25:21 +1000

    Update schemas.py

--- backend/order_service/app/schemas.py
+++ backend/order_service/app/schemas.py
@@ -1,5 +1,5 @@
 # week08/backend/order_service/app/schemas.py
-# using this file to test the backend
+# using this file to test the backend-CD
 
 from datetime import datetime
 from typing import List, Optional
